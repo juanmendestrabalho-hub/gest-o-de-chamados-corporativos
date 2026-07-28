1061d1c462cfb9a6036ba07210da3dc04072e159
juanmendestrabalho-hub <juanmendestrabalho@gmail.com>
2026-07-27 21:35:07 -0300

    Update sla.js

--- js/sla.js
+++ js/sla.js
@@ -1,1 +1,78 @@
-'
+function loadSLA(){
+
+
+const db =
+getDatabase();
+
+
+
+const container =
+document.getElementById(
+"slaCards"
+);
+
+
+
+container.innerHTML="";
+
+
+
+db.sla.forEach(item=>{
+
+
+container.innerHTML += `
+
+
+<div class="sla-card">
+
+
+<h2>
+
+${item.priority}
+
+</h2>
+
+
+<p>
+
+Tempo resposta:
+
+</p>
+
+
+<div class="time">
+
+${item.response} min
+
+</div>
+
+
+<p>
+
+Tempo solução:
+
+</p>
+
+
+<div class="time">
+
+${item.solution} min
+
+</div>
+
+
+</div>
+
+
+`;
+
+
+
+});
+
+
+}
+
+
+
+loadSLA();
