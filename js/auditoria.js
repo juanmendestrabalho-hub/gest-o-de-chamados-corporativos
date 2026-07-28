4d7d6811d635978ed61fea700e450e89556d6fb7
juanmendestrabalho-hub <juanmendestrabalho@gmail.com>
2026-07-27 21:38:22 -0300

    Update auditoria.js

--- js/auditoria.js
+++ js/auditoria.js
@@ -1,1 +1,54 @@
-'
+function loadAudit(){
+
+
+const db =
+getDatabase();
+
+
+
+const table =
+document.getElementById(
+"auditTable"
+);
+
+
+
+table.innerHTML="";
+
+
+
+db.audit
+.reverse()
+.forEach(log=>{
+
+
+table.innerHTML += `
+
+
+<tr>
+
+<td>
+${log.date}
+</td>
+
+
+<td>
+${log.action}
+</td>
+
+
+</tr>
+
+
+`;
+
+
+});
+
+
+
+}
+
+
+
+loadAudit();
