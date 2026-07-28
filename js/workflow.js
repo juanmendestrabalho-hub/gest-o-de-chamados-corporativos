f4e911f2b9befa4d3bc2d7791d5a23998ad584aa
juanmendestrabalho-hub <juanmendestrabalho@gmail.com>
2026-07-27 21:35:18 -0300

    Update workflow.js

--- js/workflow.js
+++ js/workflow.js
@@ -1,1 +1,116 @@
-'
+const WORKFLOW = [
+
+
+"Aberto",
+
+
+"Triagem",
+
+
+"Classificado",
+
+
+"Em atendimento",
+
+
+"Validação",
+
+
+"Fechado"
+
+
+];
+
+
+
+
+
+
+function nextStatus(current){
+
+
+let index =
+WORKFLOW.indexOf(current);
+
+
+
+if(index === -1)
+return WORKFLOW[0];
+
+
+
+return WORKFLOW[index+1] || current;
+
+
+}
+
+
+
+
+
+
+function advanceTicket(id){
+
+
+
+const db =
+getDatabase();
+
+
+
+const ticket =
+db.tickets.find(
+t=>t.id===id
+);
+
+
+
+if(!ticket)
+return;
+
+
+
+const old =
+ticket.status;
+
+
+
+ticket.status =
+nextStatus(
+ticket.status
+);
+
+
+
+
+ticket.history.push({
+
+
+date:
+new Date()
+.toLocaleString(),
+
+
+action:
+
+`
+Status alterado:
+
+${old}
+
+→
+
+${ticket.status}
+
+`
+
+
+});
+
+
+
+
+saveDatabase(db);
+
+
+}
