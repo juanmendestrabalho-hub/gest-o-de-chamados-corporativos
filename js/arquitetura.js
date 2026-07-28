4721d589945bdb24eb113bb5cfc0747b0cfe5767
juanmendestrabalho-hub <juanmendestrabalho@gmail.com>
2026-07-27 21:31:59 -0300

    Update arquitetura.js

--- js/arquitetura.js
+++ js/arquitetura.js
@@ -1,1 +1,100 @@
-'
+function loadArchitecture(){
+
+
+const db =
+getDatabase();
+
+
+
+const container =
+document.getElementById(
+"teams"
+);
+
+
+
+container.innerHTML="";
+
+
+
+db.teams.forEach(team=>{
+
+
+let members="";
+
+
+
+team.members.forEach(member=>{
+
+
+members += `
+
+<div class="member">
+
+👤 ${member}
+
+</div>
+
+`;
+
+
+});
+
+
+
+
+container.innerHTML += `
+
+
+<div class="team-card">
+
+
+<h3>
+
+${team.name}
+
+</h3>
+
+
+<p>
+
+Área:
+${team.area}
+
+</p>
+
+
+<p>
+
+Nível:
+${team.level}
+
+</p>
+
+
+<hr>
+
+
+<h4>
+Técnicos
+</h4>
+
+
+${members}
+
+
+</div>
+
+
+`;
+
+
+
+});
+
+
+}
+
+
+
+loadArchitecture();
